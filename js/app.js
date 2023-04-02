0dbe1da7d9ee2e4d954e80396c18bcab719fd28e
Yasmeen Mahilang <100535436+yaaassmeen@users.noreply.github.com>
2023-04-02 23:17:58 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,89 @@
+const addButton = document.querySelector("#addButton")
+const main= document.querySelector("#main")
+
+
+addButton.addEventListener(
+    "click",
+    function(){
+        addNote()
+    }
+)
+
+const saveNotes = () =>{
+    const notes = document.querySelectorAll(".note textarea");
+    console.log(notes);
+    const data = [];
+    notes.forEach(
+        (note) => {
+            data.push(note.value)
+        }
+    )
+    //console.log(data);
+    if(data.length === 0){
+        localStorage.removeItem("notes")
+    }
+    else{
+        localStorage.setItem("notes", JSON.stringify(data))
+    }
+}
+
+
+
+const addNote = (text = "") =>{
+    const note = document.createElement("div");
+    note.classList.add("note")
+    note.innerHTML = `
+    <div class="tool">
+        <i class="trash fas fa-trash"></i>
+        <i class="save fas fa-save"></i>
+    </div>
+    <textarea name="" id="" cols="30" rows="10"> ${text}</textarea>
+    `;
+
+    note.querySelector(".trash").addEventListener(
+        "click",
+        function(){
+            note.remove()
+            saveNotes();
+        }
+    )
+    note.querySelector(".save").addEventListener(
+        "click",
+        function() {
+            saveNotes()
+        }
+    )
+    note.querySelector("textarea").addEventListener(
+        "focusout",
+        function() {
+            saveNotes()
+        }
+    )
+    main.appendChild(note);
+    saveNotes()
+}
+
+(
+    function() {
+        const lsnotes = JSON.parse(localStorage.getItem("notes"));
+        if(lsnotes == null){
+            addNote()
+        }else{
+            lsnotes.forEach(
+                (lsnotes) => {
+                    addNote(lsnotes)
+                }
+            )
+        }
+        // lsnotes.forEach(
+        //     (lsnotes) => {
+        //         addNote(lsnotes)
+        //     }
+        // )
+        // if(lsnotes.length ===0){
+        //     localStorage.removeItem("notes")
+        // }else{
+        //     addNote()
+        // }
+    }
+)()
